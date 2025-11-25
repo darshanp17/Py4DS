c2d2fe4d93efc449c3492fde57b4a42d70582634
Kunal Bhoir <kunalbhoira52@gmail.com>
2025-11-25 12:01:14 +0000

    done

--- models/dptest/my_first_build.sql
+++ models/dptest/my_first_build.sql
@@ -1,21 +1,14 @@
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
 {{ config(materialized='CUSTOMER_LIST') }}
 
 select
     CUSTOMER_ID,
     FIRST_NAME,
     FIRST_ORDER_DATE,
-    MOST_RECENT_ORDER_DATE
+    MOST_RECENT_ORDER_DATE,
+    NUMBER_OF_ORDERS
 from {{ ref('DIM_CUSTOMER') }}
-where     FIRST_ORDER_DATE IS NOT NULL
+where NUMBER_OF_ORDERS > 0
 
 
 
