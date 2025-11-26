73d07916026150e389ff8de1139b4b52389f668b
Kunal Bhoir <kunalbhoira52@gmail.com>
2025-11-26 06:17:45 +0000

    done

--- models/dptest/mytest_dp.sql
+++ models/dptest/mytest_dp.sql
@@ -7,6 +7,6 @@ select
     MOST_RECENT_ORDER_DATE,
     NUMBER_OF_ORDERS
 from {{ ref('dim_customer') }}
-where NUMBER_OF_ORDERS > 0)
+where NUMBER_OF_ORDERS > 0 and CUSTOMER_ID > 50)
 
 select * from dbttest
